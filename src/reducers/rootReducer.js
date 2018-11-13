89ee22fb8e829bd03756db05d26ad334e94039f8
muscaiu <muscaiu@yahoo.com>
2018-11-13 09:31:17 +0200

    connected the Post component to the state based on url param

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,8 @@
 const initState = {
   posts: [
-    { id: 1, title: 'Post title 1', body: 'Post Body 1' },
-    { id: 2, title: 'Post title 2', body: 'Post Body 2' },
-    { id: 3, title: 'Post title 3', body: 'Post Body 3' }
+    { id: 1, title: 'Post title 1', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat in repudiandae, perferendis veniam officiis porro ducimus minima ad fuga sed maxime, velit natus aperiam magnam ipsam, hic sit architecto optio.' },
+    { id: 2, title: 'Post title 2', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ratione earum debitis minima nesciunt, itaque corrupti quibusdam ut accusantium cumque similique quaerat, temporibus commodi laudantium nihil, numquam a nisi! Corrupti.' },
+    { id: 3, title: 'Post title 3', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa earum molestiae quae enim, alias necessitatibus modi nulla amet autem molestias inventore beatae laboriosam fugit! Excepturi deleniti voluptatum facilis voluptas quos.' }
   ]
 }
 
